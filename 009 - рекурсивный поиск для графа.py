e28ebe8e8cb16bd83b94856261ea375c18dd7ba3
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2022-02-03 00:12:58 +0300

    на доработку

--- 009 - рекурсивный поиск для графа.py
+++ 009 - рекурсивный поиск для графа.py
@@ -1,4 +1,4 @@
-'''
+# сделать обход в одну сторону (включить условие: вход в ключи и из всех значений выбрать меньшее)
 def search_rec(fnd, vert_key, size):
 	print('--------------------')###
 	global cnt, stack
@@ -15,14 +15,14 @@ def search_rec(fnd, vert_key, size):
 		return 'NO'
 	elif fnd in dictor[vert_key] and cnt < size:
 		print('vetka 1')###
-		cnt -= 1
+		cnt += 1
 		print('cnt =', cnt)###
 		if not len(stack):
 			stack.extend(dictor[vert_key])
 		print('stack =',stack)###
 		vert_key = stack.pop(0)
 		print('vert_key =', vert_key)###
-		return search_rec(fnd, vert_key,size)
+		return search_rec(fnd, vert_key, size)
 	else:
 		print('vetka 2')###
 		cnt += 1
@@ -32,17 +32,7 @@ def search_rec(fnd, vert_key, size):
 		print('stack =',stack)###
 		vert_key = stack.pop(0)
 		print('vert_key =', vert_key)###
-		return search_rec(fnd, vert_key,size)
-'''
-
-def search_rec(dictor):
-	for i in dictor.values():
-		if not 1 < len(i) <3:
-			return 'NO'
-		else:
-			continue
-	else:
-		return 'YES'
+		return search_rec(fnd, vert_key, size)
 	
 # словарь смежности неорентированного графа тест1 +
 '''
@@ -58,10 +48,10 @@ dictor = {1: [2, 3],
 			3: [2, 4],
 			4: [3, 2]}'''
 # словарь смежности неорентированного графа тест3 -
-dictor = {1: [2],
+dictor = {1: [2, 4],
 			2: [1, 3],
 			3: [2, 4],
-			4: [3]}
+			4: [3, 1]}
 # словарь смежности неорентированного графа тест4 +
 '''
 dictor = {1: [2, 3]}'''
@@ -82,4 +72,4 @@ print(size)
 print('#######################')
 # s = search_rec(fnd, vert_key, size)
 # print(s)
-print(search_rec(dictor))
+print(search_rec(fnd, vert_key,size))
